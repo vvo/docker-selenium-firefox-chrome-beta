657517b8fdacd74f032a08a78aa5412520396e1d
Michael Hobbs <mh@expa.com>
2014-09-16 17:14:51 -0700

    set x11vnc passwd

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1366x768x16 &
-x11vnc -display :99 -N -forever &
+x11vnc -passwd secret -display :99 -N -forever &
 # selenium must be started by a non-root user otherwise chrome can't start
 su - chromeuser -c "start-selenium"
